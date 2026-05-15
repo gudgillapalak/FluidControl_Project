2e49f84e0da854f382baf730751ba9fafef25963
gudgillapalak <gudgillapalak123@gmail.com>
2026-05-15 20:51:19 +0530

    Fixed mobile cors issue

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,11 @@ const app = express();
 
 /* Middleware */
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 app.use(express.json());
 
